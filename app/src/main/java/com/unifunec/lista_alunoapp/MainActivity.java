663f45241cf42c56bcf0fea66c692be276290f21
EstevanVenancio <estevanvenancio12@gmail.com>
2023-02-10 20:22:17 -0300

    Add Botões

--- app/src/main/java/com/unifunec/lista_alunoapp/MainActivity.java
+++ app/src/main/java/com/unifunec/lista_alunoapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.unifunec.lista_alunoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
+
+        //Simulação de uma consulta de alunos cadastrados em um banco!
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Estevan", "Willian", "Mary Jane")
+        );
+
+        ListView lista_Alunos = findViewById(R.id.lst_Alunos);
+        lista_Alunos.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, alunos));
+
     }
 }
